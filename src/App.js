ae96bf47f3b640f86fdecb166e27df714776cd22
duznot <linanchavezleider@gmail.com>
2024-09-24 17:45:06 -0500

    generador de qr

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { QRCodeCanvas } from "qrcode.react";
+import './App.css'; // Estilos CSS
 
 function App() {
+  const [qrValue, setQrValue] = useState("");
+  const [isGenerated, setIsGenerated] = useState(false);
+
+  const handleGenerate = () => {
+    const inputText = document.getElementById("QR").value;
+    if (inputText) {
+      setQrValue(inputText);  // Generamos el QR a partir del texto del input
+      setIsGenerated(true);   // Muestra el botón de descargar
+    } else {
+      alert("Por favor, ingresa algún texto");
+    }
+  };
+
+  const handleDownload = () => {
+    const canvas = document.getElementById("qrCodeEl");
+    const pngUrl = canvas
+      .toDataURL("image/png")
+      .replace("image/png", "image/octet-stream");
+    let downloadLink = document.createElement("a");
+    downloadLink.href = pngUrl;
+    downloadLink.download = "qr-code.png";
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+    document.body.removeChild(downloadLink);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="center-container">
+      <div className="square">
+        <div className="image-container">
+          {qrValue ? (
+            <QRCodeCanvas id="qrCodeEl" value={qrValue} size={256} />
+          ) : (
+            <div className="dashed-box">Aquí va el código QR</div>
+          )}
+        </div>
+        <input
+          type="text"
+          id="QR"
+          name="qr"
+          required
+          size="40"
+          placeholder="Ingresa texto para el QR"
+        />
+        <div className="button-container">
+          <button onClick={handleGenerate}>Generar</button>
+          {isGenerated && <button onClick={handleDownload}>Descargar</button>}
+        </div>
+      </div>
     </div>
   );
 }
